2c2c2e62b11871462710d0c1d1bf865f7f2cdb8e
Astrid <me@astr.cc>
2021-04-26 16:03:06 +0200

    Fix pithcing item map

--- src/models/AdvancedStats.ts
+++ src/models/AdvancedStats.ts
@@ -17,6 +17,7 @@ export type StatName =
     | "indulgence"
     | "groundFriction"
     | "shakespearianism"
+    | "suppression"
     | "unthwackability"
     | "coldness"
     | "overpowerment"
@@ -26,10 +27,7 @@ export type StatName =
     | "tenaciousness"
     | "watchfulness"
     | "anticapitalism"
-    | "chasiness"
-
-    // this one isn't in the item list (???)
-    | "suppression";
+    | "chasiness";
 
 export class AdvancedStats {
     private constructor(private data: Record<StatName, number>) {}
@@ -37,7 +35,6 @@ export class AdvancedStats {
     static new(): AdvancedStats {
         const record: Partial<Record<StatName, number>> = {};
         for (const id of statIndices) record[id] = 0;
-        record["suppression"] = 0; // need to special case this one o.o
         return new AdvancedStats(record as Record<StatName, number>);
     }
 
@@ -168,6 +165,7 @@ const statIndices: StatName[] = [
     "indulgence",
     "groundFriction",
     "shakespearianism",
+    "suppression",
     "unthwackability",
     "coldness",
     "overpowerment",
