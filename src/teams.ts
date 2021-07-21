9e456561800c5cbeb93b0a0ca747c4dc238ebbff
Astrid <me@astr.cc>
2021-07-21 10:13:26 +0200

    Add more library teams

--- src/teams.ts
+++ src/teams.ts
@@ -63,6 +63,7 @@ export const libraryTeamIds = [
     "54d0d0f2-16e0-42a0-9fff-79cfa7c4a157", // Antarctic Fireballs
     "9494152b-99f6-4adb-9573-f9e084bc813f", // Baltimore Crabs (alt)
     "939db13f-79c9-41c5-9a15-b340b1bea875", // Boulders Bay Birds
+    "67c0a873-ef6d-4a85-8293-af638edf3c9f", // Busan Bison
     "d6a352fc-b675-40a0-864d-f4fd50aaeea0", // Canada Artists
     "2e22beba-8e36-42ba-a8bf-975683c52b5f", // Carolina Queens
     "55c9fee3-79c8-4467-8dfb-ff1e340aae8c", // Dallas Cows
@@ -77,10 +78,10 @@ export const libraryTeamIds = [
     "53d473fb-ffee-4fd3-aa1c-671228adc592", // New Hampshire Eggplants
     "cbd44c06-231a-4d1a-bb7d-4170b06e566a", // Oklahoma Heartthrobs
     "1a51664e-efec-45fa-b0ba-06d04c344628", // Oregon Psychics
-    "258f6389-aac1-43d2-b30a-4b4dde90d5eb", // [U25]
+    "258f6389-aac1-43d2-b30a-4b4dde90d5eb", // Kola Boar
     "a4b23784-0132-4813-b300-f7449cb06493", // Phoenix Trunks
     "774762ee-c234-4c57-90a1-e1e69db3f6a7", // Sao Paulo Parrots
-    "4cd14d96-f817-41a3-af6c-2d3ed0dd20b7", // [U28]
+    "4cd14d96-f817-41a3-af6c-2d3ed0dd20b7", // San Diego Saltines
     "3543229a-668c-4ac9-b64a-588422481f12", // Wyoming Dolphins
     "cfd20759-5f9c-4596-9493-2669b6daf396", // [??A]
     "4c192065-65d8-4010-8145-395f82d24ddf", // [??B]
